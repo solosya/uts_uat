eba606e22108c280fe2a13fee54ecee5ff790068
Lee Neenan <lneenan@pagemasters.com.au>
2017-04-03 17:28:37 +1000

    updated social.  added header menu hints

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -89,12 +89,11 @@ $('document').ready(function() {
         }
     });
 
-      $('#profile').on('click', function(e) {
-        
+    $('#profile').on('click', function(e) {
         $('#header__menu').toggleClass('Profile_Open');
         $('body').toggleClass('no_profile');
         e.preventDefault();
-      });
+    });
 
     cardHolder = '';
 
@@ -114,6 +113,12 @@ $('document').ready(function() {
         paginationClickable: true,
         autoplay: 20000,
         autoplayDisableOnInteraction: false
+    });
+
+    Tipped.create('.tool_tipped', {
+        maxWidth: 150,
+        offset: { x: 0, y: -35 },
+
     });
 
 
